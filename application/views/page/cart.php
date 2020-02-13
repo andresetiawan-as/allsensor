787d85f9c7acacfeac0899c32e57091ce5047322
Andre Setiawan <andresetiawan.info@gmail.com>
2020-02-13 22:26:33 +0700

    Update
    testing

--- application/views/page/cart.php
+++ application/views/page/cart.php
@@ -34,7 +34,7 @@
 					<li><a href="index.html">home</a></li>
 					<li><a href="#">woman</a></li>
 					<li><a href="#">man</a></li>
-					<li><a href="#">lookbook</a></li>
+					<li><a href="#">lookbook</a></li> 
 					<li><a href="#">blog</a></li>
 					<li><a href="#">contact</a></li>
 				</ul>
